29889f6adc59ba61435ccb872f4d0dbb38b784a8
Daniel Choi <schoiak@connect.ust.hk>
2023-09-05 09:22:35 +0900

    feat: BaseLayout
    BaseLayout 및 dependency 추가

---
+++ src/module/@base/views/BaseLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-layout ref="app">
+    <BaseHeader />
+    <main class="d-flex flex-column justify-center align-center mt-16">
+      <div style="width: 65%; border: solid 1px red">
+        <slot></slot>
+      </div>
+      <BaseFooter />
+    </main>
+  </v-layout>
+</template>
+
+<script setup>
+import { BaseFooter, BaseHeader } from '/src/module/@base/components'
+</script>
