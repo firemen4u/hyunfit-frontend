b87704d5c054af1d86fecdf735cc5cd393abf555
Yang_Seok_Jin <62278686+black-kit@users.noreply.github.com>
2023-09-05 10:05:13 +0900

    feat: TrnSearchPage 레이아웃 초기 템플릿
    Fixs: #8

---
+++ src/module/trn-search/views/TrnSearchPage.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <BasePage>
+        <div>
+            <h1>트레이너 만나보기</h1>
+        </div>
+        <div>
+
+        </div>
+    </BasePage>
+</template>
+
+<script setup>
+    import BasePage from '../views/BasePage.vue';
+</script>
+
+<style lang="">
+    
+</style>
