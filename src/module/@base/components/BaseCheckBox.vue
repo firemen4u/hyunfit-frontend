ff8ef1cdced80ed8f380dfb39763eb2c308ba10d
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-06 21:25:17 +0900

    feat: baseCheckBox 생성

--- src/module/@base/components/BaseCheckBox.vue
+++ src/module/@base/components/BaseCheckBox.vue
@@ -1,10 +1,30 @@
 <template>
-    <div>
-        <v-checkbox label="Checkbox"></v-checkbox>
-    </div>
+  <div>
+    <v-checkbox
+      :modelValue="modelValue"
+      @update:modelValue="emitUpdate"
+      :label="label"
+      :color="color"
+      :value="value"
+      :hide-details="hideDetails"
+    ></v-checkbox>
+  </div>
 </template>
-<script setup>
+
+<script>
+export default {
+  props: {
+    modelValue: Boolean, // 체크 상태를 v-model로 받음
+    label: String, // 라벨 텍스트
+    color: String, // 색상
+    value: String, // 값
+    hideDetails: Boolean, // 세부 정보 숨김 여부
+  },
+  methods: {
+    emitUpdate(value) {
+      // 체크 상태가 변경될 때 이벤트를 상위 컴포넌트에 전달
+      this.$emit('update:modelValue', value)
+    },
+  },
+}
 </script>
-<style scope>
-    
-</style>
