4e9eacc95c480cee4d6015521a0844988bffe223
Daniel Choi <schoiak@connect.ust.hk>
2023-09-07 09:58:30 +0900

    feat: main 페이지 추가

--- src/module/main/components/MainBanner.vue
+++ src/module/main/components/MainBanner.vue
@@ -1,44 +1,52 @@
 <template>
-  <div class="banner">
-    <div class="img-cover"></div>
+  <div class="banner w-100 flex items-center">
     <img
-      src="https://www.borntough.com/cdn/shop/articles/Best_Calisthenic_Workout_for_Beginners.jpg?v=1636398453"
+      src="/src/assets/images/banner.png"
       alt="Banner Image"
+      class="banner-img object-cover w-100"
     />
     <!-- 텍스트를 배너 위에 추가 -->
-    <BaseContainer></BaseContainer>
-    <div class="banner-text mt-10 ml-10">
-      <div class="text-3xl font-medium">강력한 코어를 위한</div>
-      <div class="text-4xl font-medium">오늘의 10분 운동 루틴</div>
-      <p>운동 구성</p>
-      <p>소요 시간</p>
-      <p>운동 난이도</p>
+    <div
+      class="banner-text flex justify-between w-100 h-100 items-center px-48"
+    >
+      <div>
+        <div class="text-3xl font-medium text-white">강력한 코어를 위한</div>
+        <div class="text-4xl font-medium text-white mt-1">
+          오늘의 10분 운동 루틴
+        </div>
+        <div class="flex my-6">
+          <div class="text-gray-200">
+            <p>운동 구성</p>
+            <p>소요 시간</p>
+            <p>운동 난이도</p>
+          </div>
+          <div class="ml-8 text-white font-bold">
+            <p>상체 근력 및 유산소</p>
+            <p>16분</p>
+            <p>중급</p>
+          </div>
+        </div>
+        <MainPlayButton class="mt-2" />
+      </div>
+      <MainBannerExcContainer></MainBannerExcContainer>
     </div>
   </div>
 </template>
 
 <script setup>
 import BaseContainer from '/src/module/@base/views/BaseContainer.vue'
+import MainPlayButton from './MainPlayButton.vue'
+import MainBannerExcContainer from './MainBannerExcContainer.vue'
 </script>
 
 <style scoped>
-.img-cover {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5); /* 배경 색상 및 투명도 조절 */
-}
 .banner {
   position: relative;
-  height: 400px;
-  overflow: hidden; /* 배너 내용을 넘치지 않도록 설정 */
+  height: 500px;
 }
-
-/* 배너 이미지 스타일 지정 */
-.banner img {
-  width: 100%;
-  height: auto;
-  object-fit: cover; /* 이미지가 화면에 맞게 조절되도록 설정 */
+.banner .banner-img {
+  filter: brightness(50%);
+  height: 500px;
 }
 
 /* 배너 텍스트 스타일 지정 */
@@ -46,7 +54,14 @@ import BaseContainer from '/src/module/@base/views/BaseContainer.vue'
   position: absolute;
   top: 0;
   left: 0;
-  padding: 20px; /* 텍스트와 배너 경계 사이의 여백 설정 */
-  color: #ffffff; /* 텍스트 색상 설정 */
+}
+
+.main-banner-exc-container {
+  height: 23rem;
+  width: 32rem;
+}
+.exc-vertical-card {
+  background-color: rgba(48, 48, 48, 0.4);
+  backdrop-filter: blur(10px);
 }
 </style>
