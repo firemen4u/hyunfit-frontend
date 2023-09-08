94ba600eecc3e449570368c34c69d5cf253b48bd
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-08 11:45:38 +0900

    feat: 예약내역 카드에 review modal compent 추가
    issue: #16

--- src/module/mbr-reservation/components/MbrRsvHistoryCard.vue
+++ src/module/mbr-reservation/components/MbrRsvHistoryCard.vue
@@ -9,7 +9,7 @@
       <div class="flex-col">
         <div class="text-xl font-bold">{{ trainerName }}</div>
         <div class="text-base flex text-gray-500">
-          <p>{{ `${day} | ${time}` }}</p>
+          <p>{{ `${day}    |    ${time}` }}</p>
         </div>
       </div>
     </div>
@@ -17,12 +17,28 @@
     <div
       class="card-in-card border border-3 border-solid border-gray-600 mt-3 p-5 rounded-md shadow-md"
     >
-      <div class="flex">
-        <div>{{ type }}</div>
-        <div class="ml-3">
-          <BaseRating :rating="5"></BaseRating>
+      <div class="flex justify-between">
+        <div class="flex">
+          <div>{{ type }}</div>
+          <div class="ml-3">
+            <BaseRating :rating="5"></BaseRating>
+          </div>
+        </div>
+        <div>
+          <MbrRsvReviewModal
+            @close="toggleModal"
+            :modalActive="modalActive"
+          ></MbrRsvReviewModal
+          ><button
+            class="insert-review-modal text-primary mb-2"
+            @click="toggleModal"
+            type="button"
+          >
+            리뷰 작성 하기
+          </button>
         </div>
       </div>
+
       <div class="flex justify-between text-gray-500">
         <div>{{ subTitle }}</div>
         <div>{{ ptCount }}번째 수업 완료</div>
@@ -32,7 +48,9 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { BaseRating } from '/src/module/@base/components'
+import MbrRsvReviewModal from '/src/module/mbr-reservation/components/MbrRsvReviewModal.vue'
 const props = defineProps({
   profileImageUrl: String,
   trainerName: String,
@@ -42,4 +60,20 @@ const props = defineProps({
   subTitle: String,
   ptCount: Number,
 })
+
+const showReviewModal = ref(false)
+
+const openReviewModal = () => {
+  showReviewModal.value = true
+}
+
+const closeReviewModal = () => {
+  showReviewModal.value = false
+}
+
+const modalActive = ref(false)
+
+const toggleModal = () => {
+  modalActive.value = !modalActive.value
+}
 </script>
