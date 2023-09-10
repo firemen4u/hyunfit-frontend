b6d282387b847dfd849f47af8def13a43a86d2e6
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-10 21:01:02 +0900

    feat: BaseSideBar 생성
    issue: #37

---
+++ src/module/@base/views/BaseSideBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="sidebar">
+    <!-- 사이드 바 헤더 -->
+    <SidebarHeader :sidebarTitle="sidebarTitle" />
+
+    <!-- 카테고리 -->
+    <Category :categoryTitle="categoryTitle" />
+
+    <!-- 서브카테고리 -->
+    <Subcategory
+      v-for="(subcategory, index) in subcategories"
+      :key="index"
+      :subcategoryTitle="subcategory.title"
+      :subcategoryLink="subcategory.link"
+    />
+  </span>
+</template>
+
+<script setup>
+import SidebarHeader from '/src/module/@base/components/BaseSideBarHeader.vue'
+import Category from '/src/module/@base/components/BaseSideBarCategory.vue'
+import Subcategory from '/src/module/@base/components/BaseSideBarSubcategory.vue'
+
+const props = defineProps({
+  sidebarTitle: String,
+  categoryTitle: String,
+  subcategories: Array, // 예시: [{ title: '서브 카테고리 1', link: '/link1' }, ...]
+})
+</script>
+
+<style scoped>
+/* 사이드 바 스타일링 */
+.sidebar {
+  width: 250px;
+  top: 80px;
+  margin-right: 1500px;
+  background-color: #eee;
+  position: fixed;
+  padding: 20px;
+  overflow: hidden;
+}
+</style>
