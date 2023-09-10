eabe17e9893e2b36c4ba55ad36f7931d3b54f9d0
Yang_Seok_Jin <62278686+black-kit@users.noreply.github.com>
2023-09-10 23:17:55 +0900

    feat: 타이머 컴포넌트 기능 구현
    타이머 변수를 선언해서 해당 변수에 초를 넣으면 명시된 초부터 내려감
    -> 초가 다 끝나고 나면 다음 페이지로 이동할 수 잇는 기능 구현 해야함

--- src/module/ai-training/component/AITrainingTimer.vue
+++ src/module/ai-training/component/AITrainingTimer.vue
@@ -3,7 +3,9 @@
     class="time-container absolute flex flex-col items-center justify-center top-4 right-4 bg-[#4e4e4f] w-48 h-48 rounded-full"
   >
     <p class="reps-title text-2xl font-bold text-white mb-4">Sec</p>
-    <span class="resps-count text-7xl font-bold text-white mb-4">100</span>
+    <span class="resps-count text-7xl font-bold text-white mb-4">{{
+      remainingTime
+    }}</span>
     <!-- <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240">
         <defs>
           <linearGradient id="Gradient" x1="0" x2="100%" y1="0" y2="100%">
@@ -15,7 +17,26 @@
       </svg> -->
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const initialTime = 100
+const remainingTime = ref(initialTime)
+
+const startTimer = () => {
+  const timerInterval = setInterval(() => {
+    if (remainingTime.value > 0) {
+      remainingTime.value-- // 시간을 1초씩 감소
+    } else {
+      clearInterval(timerInterval) // 시간이 0이 되면 타이머 중지
+    }
+  }, 1000) // 1초마다 업데이트
+}
+
+onMounted(() => {
+  startTimer() // 컴포넌트가 마운트되면 타이머 시작
+})
+</script>
 <style scoped>
 .circle {
   cx: 120;
