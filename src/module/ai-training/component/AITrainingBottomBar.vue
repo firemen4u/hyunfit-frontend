fcb830b1da35f57629bc2a4dd84ca1bcb06a1641
Yang_Seok_Jin <62278686+black-kit@users.noreply.github.com>
2023-09-10 23:19:59 +0900

    feat: progress bar 애니메이션 진행
    progress bar가 끝나면 자동으로 다음 페이지로 넘어갈 수 있도록 해야함

--- src/module/ai-training/component/AITrainingBottomBar.vue
+++ src/module/ai-training/component/AITrainingBottomBar.vue
@@ -15,12 +15,16 @@
         <img src="src\assets\images\volume-high.png" />
       </button>
     </div>
-    <button class="skip-button item skip">
-      <p class="font-extrabold">Skip</p>
+    <button class="skip-button skip-item">
+      <div class="progress-bar"></div>
+      <span class="skip-text font-extrabold">Skip</span>
+      다음
     </button>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+</script>
 <style scoped>
 .status-navigation-container {
   display: flex;
@@ -47,7 +51,6 @@
   width: 20px;
   height: 20px;
 }
-
 .item {
   width: 200px;
   height: 50px;
@@ -56,7 +59,50 @@
   margin: 10px;
   border-radius: 10px;
 }
-.skip {
-  padding-right: 100px;
+
+.skip-item {
+  width: 200px;
+  height: 50px;
+  background-color: rgb(33, 33, 33, 0.3);
+  margin: 10px;
+  border-radius: 10px;
+}
+
+.skip-button {
+  position: relative;
+  height: 70px;
+  width: 200px;
+  border-radius: 10px;
+  background-color: rgba(41, 41, 41, 0.8);
+  backdrop-filter: blur(5px);
+  font-size: 20px;
+  color: rgb(169, 169, 169);
+  line-height: 70px;
+  cursor: pointer;
+}
+.skip-text {
+  color: white;
+}
+
+.progress-bar {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(0, 0, 0);
+  z-index: -1;
+  border-radius: 10px;
+  animation-timing-function: linear;
+  animation-iteration-count: 1;
+  animation-duration: 10s;
+  animation-name: progress;
+}
+
+@keyframes progress {
+  0% {
+    width: 0%; /* 시작 상태에서 너비를 0%로 유지 */
+  }
+  100% {
+    width: 100%; /* 100%로 진행하여 전체 프로그레스 바 채우기 */
+  }
 }
 </style>
