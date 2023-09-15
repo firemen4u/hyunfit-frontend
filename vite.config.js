ae2efe0da7c315dd987eaba09a2dc645fc893c12
Daniel Choi <schoiak@connect.ust.hk>
2023-09-15 11:40:59 +0900

    chore: 배포설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,11 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['js-big-decimal'],
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('./private/privkey.pem'),
+      cert: fs.readFileSync('./private/fullchain.pem'),
+    },
   },
 })
