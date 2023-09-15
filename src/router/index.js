0447b98ae98d96884c9e86a77a9c2382db2794de
Daniel Choi <schoiak@connect.ust.hk>
2023-09-15 14:29:02 +0900

    fix: case 오타 수정

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import AITrainingPage from '/src/module/ai-training/view/AITrainingPage.vue'
 import TrnDetailPage from '/src/module/trn-detail/views/TrnDetailPage.vue'
 import PtRoomPage from '/src/module/PtRoom/views/PtRoomPage.vue'
 import RsvSample from '/src/module/boptrsv/views/RsvSample.vue'
-import MbrmyPage from '/src/module/mbr-mypage/views/MbrmyPage.vue'
+import MbrMyPage from '/src/module/mbr-mypage/views/MbrMyPage.vue'
 import BoRtnBoardPage from '/src/module/bo/exc/views/BoRtnBoardPage.vue'
 import BoExcNewPage from '/src/module/bo/exc/views/BoExcNewPage.vue'
 import ReportPage from '@/module/report/views/ReportPage.vue'
@@ -59,7 +59,7 @@ const router = createRouter({
     {
       path: '/mbr-myPage',
       name: 'mbr-myPage',
-      component: MbrmyPage,
+      component: MbrMyPage,
     },
     {
       path: '/bo-rtnBoard',
