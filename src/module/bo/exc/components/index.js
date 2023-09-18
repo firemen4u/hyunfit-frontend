7f3e0a2737985cc0f4bf8cc133db2d0ceab0b71e
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-18 14:45:17 +0900

    chore: 파일 이름 변경

--- src/module/bo/exc/components/index.js
+++ src/module/bo/exc/components/index.js
@@ -1,12 +1,12 @@
 import BoExcCard from './BoExcCard.vue'
-import BoFileInput from './BoFileInput.vue'
-import BoRadioButton from './BoRadioButton.vue'
+import BoExcFileInput from './BoExcFileInput.vue'
+import BoExcRadioButton from './BoExcRadioButton.vue'
 import BoExcBoardFilterContainer from './BoExcBoardFilterContainer.vue'
 import BoExcCardModal from './BoExcCardModal.vue'
 export {
   BoExcCard,
-  BoFileInput,
-  BoRadioButton,
+  BoExcFileInput,
+  BoExcRadioButton,
   BoExcBoardFilterContainer,
   BoExcCardModal,
 }
