bcc8b65b9caec37f7e39fb1c877e5745a1eadf0c
Daniel Choi <schoiak@connect.ust.hk>
2023-09-19 01:29:07 +0900

    feat: 예약 완료 페이지 추가

---
+++ src/module/pt-reservation/views/PtReservationCompletionPage.vue
@@ -0,0 +1,61 @@
+<script setup>
+import PtReservationCompletionEffect from '@/module/pt-reservation/components/PtReservationCompletionEffect.vue'
+import { BaseContainer } from '@/module/@base/views'
+import BaseBodyWrapper from '@/module/@base/views/BaseBodyWrapper.vue'
+import router from '@/router'
+
+async function gotoMbrReservation() {
+  await router.push({ name: 'mbr-reservation' })
+}
+async function gotoMain() {
+  await router.push({ name: 'main' })
+}
+</script>
+
+<template>
+  <BaseContainer>
+    <BaseBodyWrapper>
+      <div
+        class="pt-reservation-completion-container items-center justify-center flex"
+      >
+        <div
+          class="pt-reservation-completion-wrapper flex-col items-center justify-center"
+        >
+          <PtReservationCompletionEffect />
+
+          <div class="mt-10 text-xl mb-10 font-medium">
+            예약이 완료되었습니다!
+          </div>
+          <v-btn
+            :ripple="false"
+            color="primary"
+            variant="flat"
+            height="50"
+            class="w-full mt-3"
+            @click="gotoMbrReservation"
+          >
+            <div class="font-bold text-lg">MY 예약 확인하기</div>
+          </v-btn>
+          <v-btn
+            :ripple="false"
+            height="50"
+            color="primary"
+            variant="outlined"
+            class="w-full mt-3"
+            @click="gotoMain"
+          >
+            <div class="font-bold text-lg">홈으로</div>
+          </v-btn>
+        </div>
+      </div>
+    </BaseBodyWrapper>
+  </BaseContainer>
+</template>
+
+<style scoped>
+.pt-reservation-completion-wrapper {
+  display: flex;
+  width: 400px;
+  height: 620px;
+}
+</style>
