01c8fcd3c3864faae8c13bce4464888acce1030e
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-19 09:36:16 +0900

    feat: BoRtn 등록페이지 생성
    issue: #82

---
+++ src/module/bo/rtn/components/BoRtnExcListModal.vue
@@ -0,0 +1,173 @@
+<template>
+  <div
+    class="content-wrap p-2 mt-5 mb-20 shadow-md border-2 border-gray-100 rounded-lg"
+  >
+    <div class="flex justify-between items-center">
+      <BoExcBoardFilterContainer class="flex" @updateExcType="updateExcType" />
+      <div>
+        <input
+          type="text"
+          v-model="searchText"
+          placeholder=" 🔎 운동 검색"
+          class="border-2 border-solid border-gray-400 rounded-md pl-4 hover:border-gray-600"
+        />
+      </div>
+    </div>
+    <div class="exc-wrap w-full">
+      <div v-if="paginatedExercises.length > 0" class="flex flex-wrap ml-4">
+        <div v-for="exercise in paginatedExercises" :key="exercise.excSeq">
+          <BoRtnExcCard
+            :exercise="exercise"
+            @openModal="openModal"
+            @click:register="addRegisteredExercise"
+          />
+        </div>
+      </div>
+      <div v-else>해당하는 운동이 없습니다.</div>
+    </div>
+
+    <BasePagination v-model="currentPage" :total-pages="totalPages" />
+  </div>
+  <BoExcCardModal
+    :show="showModal"
+    :exercise="selectedExercise"
+    @update:show="showModal = $event"
+  />
+  <div v-for="exercise in registeredExercises" :key="exercise.excSeq">
+    운동 이름: {{ exercise.excName }}
+    <button @click="removeExercise(exercise)">X</button>
+    <!-- 삭제 버튼 -->
+  </div>
+  <div>
+    <button @click="submitExercises">선택 완료</button>
+    <button @click="clearExercises">초기화</button>
+  </div>
+</template>
+
+<script setup>
+import {
+  BoExcCard,
+  BoExcBoardFilterContainer,
+  BoExcCardModal,
+} from '/src/module/bo/exc/components'
+import BasePagination from '/src/module/@base/components/BasePagination.vue'
+import BoRtnExcCard from '/src/module/bo/rtn/components/BoRtnExcCard.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const registeredExercises = ref([]) // 등록된 운동을 저장할 변수
+const emit = defineEmits(['update:exercises']) // emit 정의
+
+// registeredExercises 배열에 운동 추가
+const addRegisteredExercise = exercise => {
+  registeredExercises.value.push(exercise)
+}
+
+// registeredExercises 배열에 운동 삭제
+const removeExercise = exerciseToRemove => {
+  const index = registeredExercises.value.findIndex(
+    exercise => exercise.excSeq === exerciseToRemove.excSeq
+  )
+  if (index !== -1) {
+    registeredExercises.value.splice(index, 1)
+  }
+}
+
+// registeredExercises 배열 초기화
+const clearExercises = () => {
+  registeredExercises.value = []
+}
+
+const submitExercises = () => {
+  emit('update:exercises', registeredExercises.value) // 상위 컴포넌트에 전달
+  console.log(
+    'Exercises sent:',
+    JSON.stringify(registeredExercises.value, null, 2)
+  ) // 콘솔에 로깅
+}
+
+const searchText = ref('') // 검색 텍스트를 저장할 ref 변수
+
+const selectedExcType = ref(null) // 선택한 excType을 저장할 변수
+
+// 검색 텍스트와 필터링된 운동 목록
+const updateExcType = value => {
+  selectedExcType.value = value // 선택한 excType 업데이트
+  console.log('Before reset:', currentPage.value) // 로그 추가
+  currentPage.value = 1 // 페이지를 1로 초기화
+  console.log('After reset:', currentPage.value) // 로그 추가
+}
+
+const itemsPerPage = ref(5) // 한 페이지당 표시될 아이템 수
+const currentPage = ref(1) // 현재 페이지
+
+const totalPages = computed(() => {
+  return Math.ceil(filteredExercises.value.length / itemsPerPage.value)
+})
+// 페이지에 따라 나눌 운동 목록
+const paginatedExercises = computed(() => {
+  const start = (currentPage.value - 1) * itemsPerPage.value
+  const end = start + itemsPerPage.value
+  return filteredExercises.value.slice(start, end)
+})
+
+const filteredExercises = computed(() => {
+  let result = exercises.value
+
+  // excType에 따른 필터링
+  if (selectedExcType.value !== null) {
+    result = result.filter(
+      exercise => exercise.excType === selectedExcType.value
+    )
+  }
+
+  // 검색어에 따른 필터링
+  if (searchText.value) {
+    result = result.filter(exercise => {
+      return exercise.excName
+        .toLowerCase()
+        .includes(searchText.value.toLowerCase())
+    })
+  }
+
+  return result
+})
+
+const exercises = ref([]) // API로 받아온 운동 목록을 저장할 변수
+// API를 통해 운동 목록을 가져오는 함수
+const fetchExercises = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/exercises') // API 엔드포인트를 설정하세요.
+    const data = await response.json()
+    exercises.value = data // 받아온 데이터를 exercises 변수에 저장합니다.
+  } catch (error) {
+    console.error('운동 목록을 불러오는 중 에러 발생:', error)
+  }
+}
+
+// 컴포넌트가 마운트된 후 API로부터 데이터를 가져옵니다.
+onMounted(() => {
+  fetchExercises()
+})
+
+watch(searchText, () => {
+  currentPage.value = 1 // 검색어가 바뀔 때 페이지를 1로 초기화
+  console.log(
+    '검색어 바뀌어서 1로초기화 currentPage.value :',
+    currentPage.value
+  )
+})
+
+watch(currentPage, (newVal, oldVal) => {
+  console.log('BoExcBoardPage currentPage changed:', newVal, oldVal) // 로그 추가
+})
+
+//모달창
+const showModal = ref(false) // 모달 상태를 관리할 변수
+const selectedExercise = ref(null) // 선택된 운동을 저장할 변수
+
+// 모달을 띄우는 함수
+const openModal = exercise => {
+  selectedExercise.value = exercise
+  showModal.value = true
+}
+</script>
---
+++ src/module/bo/rtn/views/BoRtnNewPage.vue
@@ -0,0 +1,284 @@
+<template lang="">
+  <BaseContainer>
+    <BaseSideBar
+      :sidebarHeader="sidebarHeader"
+      :categoryTitle="mainCategory"
+      :subcategories="subcategories"
+    />
+    <BaseBodyWrapper>
+      <div class="flex justify-between">
+        <div class="text-3xl font-bold">루틴 생성</div>
+      </div>
+
+      <div class="rtn-wrap border-4 border-dashed border-blue-500">
+        <div class="rtn-name">
+          <p class="col-1">루틴 이름</p>
+          <v-text-field
+            v-model="exc_name"
+            clearable
+            maxlength="20"
+            placeholder="20자 내로 작성하세요"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </div>
+
+        <div class="rtn-content">
+          <p class="col-1">루틴 설명</p>
+          <v-textarea
+            v-model="exc_content"
+            clearable
+            placeholder="50자 내로 작성하세요"
+            maxlength="50"
+            variant="solo"
+            single-line
+          ></v-textarea>
+        </div>
+
+        <div class="rtn-thumnail">
+          <p>루틴 썸네일</p>
+          <div>
+            <v-file-input
+              label="File input"
+              v-model="files"
+              @change="previewImage"
+            ></v-file-input>
+            <div v-if="imageUrl">
+              <img :src="imageUrl" alt="Image Preview" width="200" />
+            </div>
+          </div>
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">루틴 타겟 부위</p>
+          <BoExcRadioButton
+            :options="rtn_target_radio"
+            v-model="rtn_target"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">루틴 난이도</p>
+          <BoExcRadioButton
+            :options="rtn_experience_level_radio"
+            v-model="rtn_experience_level"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">루틴 진행 시간</p>
+          <BoExcRadioButton
+            :options="rtn_duration_radio"
+            v-model="rtn_duration"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">운동 목표</p>
+          <BoExcRadioButton
+            :options="rtn_goal_radio"
+            v-model="rtn_goal"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">무릎 운동 고려</p>
+          <BoExcRadioButton
+            :options="rtn_knee_health_considered_radio"
+            v-model="rtn_knee_health_considered"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">층간 소음 고려</p>
+          <BoExcRadioButton
+            :options="rtn_noise_considered_radio"
+            v-model="rtn_noise_considered"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">오래 앉아 있는 사람</p>
+          <BoExcRadioButton
+            :options="rtn_long_sitter_radio"
+            v-model="rtn_long_sitter"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">목 어깨 포커스</p>
+          <BoExcRadioButton
+            :options="rtn_neck_shoulder_focused_radio"
+            v-model="rtn_neck_shoulder_focused"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">허리 디스크 고려</p>
+          <BoExcRadioButton
+            :options="rtn_back_disk_considered_radio"
+            v-model="rtn_back_disk_considered"
+            hide-details
+          />
+        </div>
+
+        <div class="flex items-center mt-4">
+          <p class="col-1">포인트</p>
+          <v-text-field
+            v-model="rtn_reward_point"
+            clearable
+            placeholder="루틴 완료시 지급 포인트"
+            hide-details
+            variant="solo"
+            suffix="P"
+          ></v-text-field>
+        </div>
+      </div>
+
+      <div class="border-4 border-dashed border-green-500">
+        <button @click="toggleModal">운동 목록 보기</button>
+
+        <BoRtnExcListModal
+          v-if="showModal"
+          @update:show="showModal = $event"
+          @update:exercises="updateExercises"
+        />
+      </div>
+
+      <div><button @click="sendDataToAPI">루틴 등록하기 버튼</button></div>
+    </BaseBodyWrapper>
+  </BaseContainer>
+</template>
+<script setup>
+import { BaseBodyWrapper, BaseContainer } from '/src/module/@base/views'
+import BaseSideBar from '/src/module/@base/views/BaseSideBar.vue'
+import BasePagination from '/src/module/@base/components/BasePagination.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+import BoRtnExcListModal from '/src/module/bo/rtn/components/BoRtnExcListModal.vue'
+import { BoExcFileInput, BoExcRadioButton } from '/src/module/bo/exc/components'
+
+const sidebarHeader = '관리페이지'
+const mainCategory = 'AI 트레이닝'
+const subcategories = [
+  { id: 1, title: '운동 관리', link: '/bo-excBoard' },
+  { id: 2, title: '루틴 관리', link: '/bo-rtnboard' },
+  { id: 3, title: '운동 등록', link: '/bo-excNew' },
+  { id: 4, title: '루틴 등록', link: '/link4' },
+]
+
+// 루틴 타켓부위
+const rtn_target_radio = [
+  { label: '상체', value: '1' },
+  { label: '하체', value: '2' },
+  { label: '전신', value: '3' },
+  { label: '유산소', value: '4' },
+]
+
+// 루틴 난이도
+const rtn_experience_level_radio = [
+  { label: '초급', value: '1' },
+  { label: '초중급', value: '2' },
+  { label: '중급', value: '3' },
+  { label: '중상급', value: '4' },
+  { label: '상급', value: '5' },
+]
+
+// 루틴 진행시간
+const rtn_duration_radio = [
+  { label: '10-15분', value: '1' },
+  { label: '15-20분', value: '2' },
+  { label: '20-25분', value: '3' },
+]
+
+// 운동 목표
+const rtn_goal_radio = [
+  { label: '1번 목표', value: '1' },
+  { label: '2번 목표', value: '2' },
+]
+
+// 무릎 운동 고려
+const rtn_knee_health_considered_radio = [
+  { label: '예', value: '1' },
+  { label: '아니오', value: '2' },
+]
+
+// 층간 소음 고려
+const rtn_noise_considered_radio = [
+  { label: '예', value: '1' },
+  { label: '아니오', value: '2' },
+]
+
+// 오래 앉아 있는 사람
+const rtn_long_sitter_radio = [
+  { label: '예', value: '1' },
+  { label: '아니오', value: '2' },
+]
+
+// 목 어깨 포커스
+const rtn_neck_shoulder_focused_radio = [
+  { label: '예', value: '1' },
+  { label: '아니오', value: '2' },
+]
+
+// 허리 디스크 고려
+const rtn_back_disk_considered_radio = [
+  { label: '예', value: '1' },
+  { label: '아니오', value: '2' },
+]
+
+//submit 시키기
+const rtn_goal = ref('')
+const rtn_target = ref('')
+const rtn_experience_level = ref('')
+const rtn_knee_health_considered = ref('')
+const rtn_noise_considered = ref('')
+const rtn_long_sitter = ref('')
+const rtn_neck_shoulder_focused = ref('')
+const rtn_back_disk_considered = ref('')
+
+// 운동 목록 모달 보여주는 곳
+const showModal = ref(false)
+const toggleModal = () => {
+  showModal.value = !showModal.value
+}
+
+const files = ref([]) // 선택한 파일을 저장할 ref 변수
+const imageUrl = ref(null) // 이미지 미리보기 URL을 저장할 ref 변수
+
+// 파일 미리보기를 생성하는 함수
+const previewImage = () => {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => {
+    imageUrl.value = reader.result
+  })
+
+  if (files.value && files.value[0]) {
+    reader.readAsDataURL(files.value[0])
+  }
+}
+
+const exercisesFromModal = ref([]) // 모달에서 받은 exercises
+
+const updateExercises = updatedExercises => {
+  exercisesFromModal.value = updatedExercises
+}
+
+const sendDataToAPI = () => {
+  const payload = {
+    // ... 다른 필드
+    exercises: exercisesFromModal.value,
+  }
+
+  // API 전송 로직
+  console.log('Data sent to API:', JSON.stringify(payload, null, 2)) // 콘솔에 로깅
+}
+</script>
+<style scoped></style>
