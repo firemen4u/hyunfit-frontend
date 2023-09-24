ba053dc01181009757f198114e2ea3b240dfa382
tigre911 <tmvhswlqkqn_@naver.com>
2023-09-25 02:12:26 +0900

    feat: ExcCard Video add
    issue: #128

--- src/module/bo/exc/components/BoExcCard.vue
+++ src/module/bo/exc/components/BoExcCard.vue
@@ -4,10 +4,13 @@
     @click="handleClick"
   >
     <div>
-      <img
+      <!-- <img
         class="exc-img"
         src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdAJq71%2FbtsteQJsEa7%2FntSCTKbwomKjVIpQusuLwk%2Fimg.jpg"
-      />
+      /> -->
+      <video controls loop muted autoplay class="exc-img">
+        <source :src="videoSrc" />
+      </video>
     </div>
     <div class="exc-name">운동 이름 : {{ exercise.excName }}</div>
     <div class="exc-content">운동 설명 : {{ exercise.excContent }}</div>
@@ -15,8 +18,10 @@
 </template>
 
 <script setup>
+import { FILE_SERVER_HYUNFIT_URL } from '/src/config.js'
+import { computed } from 'vue'
 const props = defineProps({
-  exercise: Object, 
+  exercise: Object,
 })
 
 const emit = defineEmits([])
@@ -24,6 +29,12 @@ const emit = defineEmits([])
 const handleClick = () => {
   emit('openModal', props.exercise)
 }
+
+// 계산된 속성을 사용하여 비디오 URL을 생성
+const videoSrc = computed(
+  () =>
+    `${FILE_SERVER_HYUNFIT_URL}/low_quality_preview_video_${props.exercise.excSeq}.mp4`
+)
 </script>
 
 <style scoped>
